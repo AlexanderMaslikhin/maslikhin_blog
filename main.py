4eb189c7172ca73432701c1c2d985976ec533ce9
AlexanderMaslikhin <shu75@yandex.ru>
2022-09-20 23:15:45 +0300

    blueprint subdomain

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from flask import Flask
 from flask import Blueprint
+from flask import Flask
+
 app = Flask(__name__)
 app.config['SERVER_NAME'] = 'maslikhin.ru'
 
 bp = Blueprint('subdomain', __name__, subdomain='dl')
 
 
-
 @bp.route('/')
 def index():
     return '<img src="./test_imgs/1.jpg></img>'
 
-app.route("/")
+
+@app.route("/")
 def hello():
     return "<h1 style='color:blue'>Coming soon...</h1><i>Скоро тут что-то будет... Но это не точно</i>"
+
 
 app.register_blueprint(bp)
 
